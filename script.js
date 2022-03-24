159be8d2882ce1aeb9cf9216985b00adc0ad0672
sf-002 <sf-002@ya.ru>
2022-03-24 21:02:05 +0300

    10th practice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Ilia";
-const myStr = "My name is "+myName+" and I am well!";
+// Change code below this line
+const someAdjective = "fdfzasdf";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
